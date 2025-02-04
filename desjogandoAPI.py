c3990455f4780f64e7e00b0a197d5a1978316f2f
Gustavo Hetrich <gustavohetrich@outlook.com>
2025-02-04 13:24:18 -0300

    .

--- desjogandoAPI.py
+++ desjogandoAPI.py
@@ -2,18 +2,25 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Dict
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 
 # Servindo arquivos estáticos da pasta 'static'
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Rota para servir o arquivo index.html
 @app.get("/")
 async def read_index():
-    with open("static/index.html", "r") as f:
-        return f.read()
+    index_path = os.path.join(os.path.dirname(__file__), "static", "index.html")
+    
+    # Verificar se o arquivo existe antes de tentar servi-lo
+    if os.path.exists(index_path):
+        with open(index_path, "r") as f:
+            return f.read()
+    else:
+        return {"detail": "index.html não encontrado"}
 
 # Adicionando o CORS para permitir chamadas de qualquer origem
 app.add_middleware(
