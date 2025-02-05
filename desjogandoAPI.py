0d54348ce329fee1f50da23a8626037844173651
Gustavo Hetrich <gustavohetrich@outlook.com>
2025-02-05 10:25:11 -0300

    .

--- desjogandoAPI.py
+++ desjogandoAPI.py
@@ -65,6 +65,12 @@ async def startup():
         escolha INTEGER
     );
     ''')
+    # Insere um registro inicial na tabela saldo_apostas se não existir
+    await conn.execute('''
+    INSERT INTO saldo_apostas (id, saldo_total)
+    VALUES (1, 0)
+    ON CONFLICT (id) DO NOTHING;
+    ''')
     await conn.close()
 
 # Rota para login do usuário
